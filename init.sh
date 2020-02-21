3ccab4418fc7c51214dc2075cc0afcd56b32cb43
Hugo Lafleur <handspell@gmail.com>
2020-02-21 16:18:44 -0500

    cosmetic changes

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/zsh
 set -e
 
 # install homebrew
 if [[ -f `which brew` ]]; then
-   echo "updating homebrew"
+   echo -e "\e[1;33m*** updating homebrew ***\e[0m"
    brew update
    echo
-   echo "upgrading homebrew packages"
+   echo -e "\e[1;33m*** upgrading homebrew packages ***\e[0m"
    brew upgrade
    echo
-   echo "cleaning up homebrew"
+   echo -e "\e[1;33m*** cleaning up homebrew ***\e[0m"
    brew cleanup
 else
    echo "installing homebrew"
@@ -19,17 +19,17 @@ fi
 
 # install homebrew packages
 echo
-echo "installing homebrew packages in homebrew.lst"
+echo -e "\e[1;33m*** installing homebrew packages in homebrew.lst ***\e[0m"
 cat homebrew.lst | xargs brew install
 
 # install python packages
 echo
-echo "installing python packages in requirements.txt"
+echo -e "\e[1;33m*** installing python packages in requirements.txt ***\e[0m"
 pip3 install -r requirements.txt
 
 # install dotfiles
 echo
-echo "linking dotfiles"
+echo "\e[1;33m*** linking dotfiles ***\e[0m"
 cd dotfiles
 for dotfile in `find . -type f -maxdepth 1 |sort| cut -d/ -f2`
 do
