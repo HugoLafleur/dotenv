7243e9b5e646d0ab43f354b2f7385f2f46612687
Hugo Lafleur <handspell@gmail.com>
2020-03-02 15:02:35 -0500

    cosmetics

--- init.sh
+++ init.sh
@@ -3,14 +3,14 @@ set -e
 
 # install homebrew
 if [[ -f `which brew` ]]; then
-   echo -e "\e[1;33m*** updating homebrew ***\e[0m"
-   brew update
+   echo -e "\e[1;33m+++ updating homebrew +++\e[0m"
+   brew update --verbose
    echo
-   echo -e "\e[1;33m*** upgrading homebrew packages ***\e[0m"
-   brew upgrade
+   echo -e "\e[1;33m+++ upgrading homebrew packages +++\e[0m"
+   brew upgrade --verbose
    echo
-   echo -e "\e[1;33m*** cleaning up homebrew ***\e[0m"
-   brew cleanup
+   echo -e "\e[1;33m+++ cleaning up homebrew +++\e[0m"
+   brew cleanup --verbose
 else
    echo "installing homebrew"
    /usr/bin/ruby -e \
@@ -19,20 +19,20 @@ fi
 
 # install homebrew packages
 echo
-echo -e "\e[1;33m*** installing homebrew packages in homebrew.lst ***\e[0m"
+echo -e "\e[1;33m+++ installing homebrew packages in homebrew.lst +++\e[0m"
 cat homebrew.lst | xargs brew install
 
 # install python packages
 echo
-echo -e "\e[1;33m*** installing python packages in requirements.txt ***\e[0m"
+echo -e "\e[1;33m+++ installing python packages in requirements.txt +++\e[0m"
 pip3 install -r requirements.txt
 
 # install dotfiles
 echo
-echo "\e[1;33m*** linking dotfiles ***\e[0m"
+echo "\e[1;33m+++ linking dotfiles +++\e[0m"
 cd dotfiles
 for dotfile in `find . -type f -maxdepth 1 |sort| cut -d/ -f2`
 do
-   echo "   dotfiles/${dotfile} ->" ~/.${dotfile}
+   echo "dotfiles/${dotfile} ->" ~/.${dotfile}
    ln -fs $(pwd)/${dotfile} ~/.${dotfile}
 done
