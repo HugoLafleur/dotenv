825920afb08560d7ab79288b3cc74f8be706ed50
Hugo Lafleur <hugo.lafleur@gmail.com>
2021-01-14 12:15:55 -0500

    add intermediary steps

--- init.sh
+++ init.sh
@@ -27,6 +27,22 @@ echo
 echo -e "\e[1;33m+++ installing python packages in requirements.txt +++\e[0m"
 pip3 install -r requirements.txt
 
+# install prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# install tat script
+curl https://raw.githubusercontent.com/christoomey/dotfiles/master/bin/tat > ~/workspace/.env/bin/tat
+
+# install vim plugins
+ln -fs ~/workspace/.env/dotfiles/vim ~/.vim
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # install dotfiles
 echo
 echo "\e[1;33m+++ linking dotfiles +++\e[0m"
